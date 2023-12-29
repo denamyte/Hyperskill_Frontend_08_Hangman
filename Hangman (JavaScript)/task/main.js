e20dec322e91a8326883483896e06fbc3c8522f4
denys <denamyte@gmail.com>
2023-12-29 23:04:48 +0200

    2023-12-29: Stage 6/8

--- Hangman (JavaScript)/task/main.js
+++ Hangman (JavaScript)/task/main.js
@@ -1,10 +1,10 @@
 const input = require('sync-input')
 
+let attemptCount = 8;
 const words = ["python", "java", "swift", "javascript"];
 const word = words[Math.floor(Math.random() * words.length)];
 const knownLetters = [];
 const welcome = () => console.log("H A N G M A N\n");
-const thanks = () => console.log("Thanks for playing!");
 const ask = msg => input(msg);
 const getWordMask = () =>
     word.split('')
@@ -16,21 +16,37 @@ function checkLetter(letter) {
     if (word.includes(letter)) {
         if (!knownLetters.includes(letter))
             knownLetters.push(letter);
-        console.log(knownLetters);
+        else {
+            console.log("No improvements.");
+            attemptCount--;
+        }
     } else {
         console.log("That letter doesn't appear in the word.");
+        attemptCount--;
     }
 }
 
+const win = () => word
+    .split("")
+    .every(letter => knownLetters.includes(letter));
+const lost = () => attemptCount <= 0;
+
 function main() {
-    let attemptCount = 8;
     welcome();
-    while (attemptCount-- > 0) {
-        console.log(getWordMask())
+    while (true) {
+        if (lost()) {
+            console.log("You lost!");
+            break;
+        } else if (win()) {
+            console.log(word +
+                "\nYou guessed the word!" +
+                "\nYou survived!");
+            break;
+        }
+        console.log(getWordMask());
         checkLetter(ask("Input a letter: "));
         console.log("");
     }
-    thanks();
 }
 
 main()
