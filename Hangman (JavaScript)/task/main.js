c85d5e335d79c6687b2c601b56b3438038985ed8
denys <denamyte@gmail.com>
2023-12-29 14:15:11 +0200

    2023-12-29: Stage 2/8

--- Hangman (JavaScript)/task/main.js
+++ Hangman (JavaScript)/task/main.js
@@ -1,4 +1,15 @@
 const input = require('sync-input')
 
-console.log("H A N G M A N\n" +
-    "The game will be available soon.");
+const word = "python";
+const welcome = () => console.log("H A N G M A N");
+const ask = msg => input(msg);
+const defineRes = userInp => userInp === word;
+const showRes = flag => console.log(flag ? "You survived!" : "You lost!");
+
+function main() {
+    welcome();
+    const userInp = ask("Guess the word: ");
+    showRes(defineRes(userInp))
+}
+
+main()
