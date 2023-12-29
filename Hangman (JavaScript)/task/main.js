1d76ac1e38774271628f628f9d37132e665ca126
denys <denamyte@gmail.com>
2023-12-29 15:27:36 +0200

    2023-12-29: Stage 4/8

--- Hangman (JavaScript)/task/main.js
+++ Hangman (JavaScript)/task/main.js
@@ -1,15 +1,22 @@
 const input = require('sync-input')
 
 const words = ["python", "java", "swift", "javascript"];
-const index = Math.floor(Math.random() * words.length);
+const word = words[Math.floor(Math.random() * words.length)];
+const knownIndices = [0, 1, 2];
 const welcome = () => console.log("H A N G M A N");
 const ask = msg => input(msg);
-const defineRes = userInp => userInp === words[index];
+const getWordMask = () =>
+    word.split('')
+        .map((letter, i) =>
+            i in knownIndices ? letter : '-')
+        .join("");
+const defineRes = userInp => userInp === word;
 const showRes = flag => console.log(flag ? "You survived!" : "You lost!");
 
 function main() {
     welcome();
-    const userInp = ask("Guess the word: ");
+    const mask = getWordMask();
+    const userInp = ask(`Guess the word ${mask}: `);
     showRes(defineRes(userInp))
 }
 
