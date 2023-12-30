54934016c11adae0cb42b039c99b77ea760a3c87
denys <denamyte@gmail.com>
2023-12-30 22:32:05 +0200

    2023-12-30: Stage 7/8: finished

--- Hangman (JavaScript)/task/main.js
+++ Hangman (JavaScript)/task/main.js
@@ -1,50 +1,59 @@
 const input = require('sync-input')
 
 let attemptCount = 8;
+const lowRegex = /[a-z]/;
 const words = ["python", "java", "swift", "javascript"];
 const word = words[Math.floor(Math.random() * words.length)];
-const knownLetters = [];
+const guessedLetters = [];
 const welcome = () => console.log("H A N G M A N\n");
-const ask = msg => input(msg);
+const ask = () => input("Input a letter: ");
 const getWordMask = () =>
     word.split('')
         .map((letter) =>
-            knownLetters.includes(letter) ? letter : '-')
+            guessedLetters.includes(letter) ? letter : '-')
         .join("");
 
+function getError(letter) {
+    return letter.length !== 1
+        ? "Please, input a single letter."
+        : !lowRegex.test(letter)
+            ? "Please, enter a lowercase letter from the English alphabet."
+            : guessedLetters.includes(letter)
+                ? "You've already guessed this letter."
+                : "";
+}
+
 function checkLetter(letter) {
-    if (word.includes(letter)) {
-        if (!knownLetters.includes(letter))
-            knownLetters.push(letter);
-        else {
-            console.log("No improvements.");
-            attemptCount--;
-        }
-    } else {
+    const error = getError(letter);
+    if (error) {
+        console.log(error)
+        return;
+    }
+    if (!word.includes(letter)) {
         console.log("That letter doesn't appear in the word.");
         attemptCount--;
     }
+    guessedLetters.push(letter);
 }
 
 const lost = () => attemptCount <= 0;
 const win = () => word
     .split("")
-    .every(letter => knownLetters.includes(letter));
+    .every(letter => guessedLetters.includes(letter));
+const result = () => lost() && "You lost!" ||
+    win() && `You guessed the word ${word}!\nYou survived!` ||
+    null;
 
 function main() {
     welcome();
     while (true) {
-        if (lost()) {
-            console.log("You lost!");
-            break;
-        } else if (win()) {
-            console.log(word +
-                "\nYou guessed the word!" +
-                "\nYou survived!");
+        console.log(getWordMask());
+        checkLetter(ask());
+        const res = result();
+        if (res) {
+            console.log(res);
             break;
         }
-        console.log(getWordMask());
-        checkLetter(ask("Input a letter: "));
         console.log("");
     }
 }
