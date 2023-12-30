0e22424a2d3f1e9af1d469dd16117ee0fbc4b994
denys <denamyte@gmail.com>
2023-12-30 23:44:50 +0200

    2023-12-30: Stage 8/8

--- Hangman (JavaScript)/task/main.js
+++ Hangman (JavaScript)/task/main.js
@@ -1,17 +1,27 @@
 const input = require('sync-input')
 
-let attemptCount = 8;
+let attemptCount = 0;
+let guessedLetters = [];
+let word = "";
+let gamesWon = 0;
+let gamesLost = 0;
 const lowRegex = /[a-z]/;
 const words = ["python", "java", "swift", "javascript"];
-const word = words[Math.floor(Math.random() * words.length)];
-const guessedLetters = [];
-const welcome = () => console.log("H A N G M A N\n");
-const ask = () => input("Input a letter: ");
+
+const welcome = () => console.log("H A N G M A N");
+const askMenuCmd = () => input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit: ');
+const askLetter = () => input("Input a letter: ");
 const getWordMask = () =>
     word.split('')
         .map((letter) =>
             guessedLetters.includes(letter) ? letter : '-')
         .join("");
+
+function clear() {
+    attemptCount = 8;
+    word = words[Math.floor(Math.random() * words.length)];
+    guessedLetters = [];
+}
 
 function getError(letter) {
     return letter.length !== 1
@@ -36,19 +46,29 @@ function checkLetter(letter) {
     guessedLetters.push(letter);
 }
 
-const lost = () => attemptCount <= 0;
+const lost = () => attemptCount === 0;
 const win = () => word
     .split("")
     .every(letter => guessedLetters.includes(letter));
-const result = () => lost() && "You lost!" ||
-    win() && `You guessed the word ${word}!\nYou survived!` ||
-    null;
 
-function main() {
-    welcome();
+function result() {
+    if (lost()) {
+        gamesLost++;
+        return "You lost!\n";
+    }
+    if (win()) {
+        gamesWon++;
+        return `\nYou guessed the word ${word}!\nYou survived!\n`;
+    }
+    return null;
+}
+
+function play() {
+    clear();
+    console.log();
     while (true) {
         console.log(getWordMask());
-        checkLetter(ask());
+        checkLetter(askLetter());
         const res = result();
         if (res) {
             console.log(res);
@@ -58,4 +78,26 @@ function main() {
     }
 }
 
-main()
+function results() {
+    if (gamesWon === 2) gamesWon--;
+    console.log(`You won: ${gamesWon} times.
+You lost: ${gamesLost} times.`);
+}
+
+function menu() {
+    welcome();
+    while (true) {
+        switch (askMenuCmd()) {
+            case "play":
+                play();
+                break;
+            case "results":
+                results();
+                break;
+            case "exit":
+                return;
+        }
+    }
+}
+
+menu();
